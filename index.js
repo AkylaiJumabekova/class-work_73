67bd4c98a36454ff1d7fa5d65ecf157326a02af5
AkylaiJumabekova <jumabekovaakylai7@gmail.com>
2024-08-13 17:39:14 +0600

    добавила строки кода с динамическими данными в index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/about', (req, res) => {
     return res.send('<h1>About</h1>');
 })
 
+app.get('/hello/:name', (req, res) => {
+    return res.send(`Hello, ${req.params.name}`);
+}) // мы можем часть роута сделать динамической как и в реакте, часть роута может начаться с : и тем самым все после hello и : будет попадать в этот роут, name может быть произвольным (мы можем написать userName и разницы никакой не будет, но нужно будет и при get и при send использовать одно название, так как это ключ )
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 })
